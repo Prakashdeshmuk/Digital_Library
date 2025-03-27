f1aa7c2b40de5c1e297bb60a114a8d1f78af005b
Prakashdeshmuk <suhasdeshmukh100101@gmail.com>
2025-03-27 13:19:41 +0530

    fetch borrowed books from database and update BookList component

--- app/(root)/my-profile/page.tsx
+++ app/(root)/my-profile/page.tsx
@@ -1,10 +1,37 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
-import { signOut } from "@/auth";
+import { auth, signOut } from "@/auth";
 import BookList from "@/components/BookList";
 import { sampleBooks } from "@/constants";
+import { books, borrowRecords, users } from "@/database/schema";
+import { db } from "@/database/drizzle";
+import { eq, desc } from "drizzle-orm";
 
-const Page = () => {
+const Page = async () => {
+  const session = await auth();
+
+  const result = (await db
+    .select({
+      id: books.id,
+      title: books.title,
+      author: books.author,
+      genre: books.genre,
+      rating: books.rating,
+      coverUrl: books.coverUrl,
+      coverColor: books.coverColor,
+      description: books.description,
+      totalCopies: books.totalCopies,
+      availableCopies: books.availableCopies,
+      videoUrl: books.videoUrl,
+      summary: books.summary,
+      createdAt: books.createdAt,
+    })
+    .from(borrowRecords)
+    .innerJoin(books, eq(borrowRecords.bookId, books.id))
+    .where(session?.user?.id ? eq(borrowRecords.userId, session.user.id) : undefined)
+    .orderBy(desc(borrowRecords.borrowDate))) as Book[];
+
+  console.log(result);
   return (
     <>
       <form
@@ -18,7 +45,7 @@ const Page = () => {
         <Button>Logout</Button>
       </form>
 
-      <BookList title="Borrowed Books" books={sampleBooks} />
+      <BookList title="Borrowed Books" books={result} />
     </>
   );
 };
